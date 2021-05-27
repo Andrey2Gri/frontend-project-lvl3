62fe523306898f59ed09c1c1d495e7fd22e214e0
andrey <andrey2gri@gmail.com>
2021-05-27 19:27:44 +0300

    add routes in getData

--- src/getData.js
+++ src/getData.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 import parse from './parser.js';
 
+const routes = {
+  allOrigins: (url) => {
+    const result = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
+    result.searchParams.set('url', url);
+    result.searchParams.set('disableCache', 'true');
+    return result.toString();
+  },
+};
+
 export default (url) => axios
-  .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
+  .get(routes.allOrigins(url))
   .then((response) => parse(response.data.contents))
   .catch((e) => {
     throw e;
